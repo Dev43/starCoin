1b10c2893ebbd977de0731d0ba672fc5c302e985
Dev43 <patrick.guay43@gmail.com>
2017-07-22 23:06:48 -0400

    add necessary packages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
+var bitcoin = require( 'bitcoin-promise' ) ;
 var router = express.Router();
-
-/* GET home page. */
+/* POST with data */
 router.get('/', function(req, res, next) {
+
+  var client = new bitcoin.Client({
+    host: '159.203.87.162',
+    port: 80,
+    user: 'user',
+    pass: 'user',
+    timeout: 30000
+  });
+  
+  client.cmd('getinfo', function(err, balance, resHeaders){
+  if (err) return console.log(err);
+  console.log('Balance:', balance);
+  });
+
+  
+
   res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
+
+
